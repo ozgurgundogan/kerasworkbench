3c2a32796eb804b20dc0f72faaf150d1bd478e65
ozgur <ozgurgundogan90@gmail.com>
2017-08-20 11:36:55 +0300

    new

--- keras_dropoutsuz.py
+++ keras_dropoutsuz.py
@@ -9,16 +9,13 @@ import numpy as np
 from numpy import genfromtxt
 
 model = Sequential()
-model.add(Dense(10000,input_shape=(80000)))
+model.add(Dense(10000, input_dim=80000))
 model.add(Activation('relu'))
 model.add(Dense(2000))
 model.add(Activation('relu'))
 model.add(Dense(300))
 model.add(Activation('relu'))
-model.add(Dense(3))
-
-def remove_zero(s):
-    return s[:-1]
+model.add(Dense(3, activation='relu'))
 
 for root, dirs, files in os.walk("/home/ozgurgundogan/Desktop/satellitedata/"): #directory
 #for root, dirs, files in os.walk(r"C:\Users\hilmi\Desktop\keras/"): #directory
@@ -31,11 +28,11 @@ for root, dirs, files in os.walk("/home/ozgurgundogan/Desktop/satellitedata/"): 
                 g3 = float(c[:-4])
             ground_truth = np.array([g1,g2,g3]) # ground truth we obtain from .txt
             my_data = genfromtxt("/home/ozgurgundogan/Desktop/satellitedata/" + ff, delimiter='\n')
-            print (my_data.size) 
+    	    my_data = my_data.transpose() 
             
             # stochastic gradient descent 
 	    sgd = keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
             
             model.compile(loss='mse', optimizer = sgd ,metrics = ['accuracy'])
-            histry = model.fit(my_data,ground_truth,epochs = 1, batch_size = 1,verbose=2)
+            histry = model.fit(my_data.flatten(),ground_truth,epochs = 1, batch_size = 1,verbose=2)
 	    print histry
