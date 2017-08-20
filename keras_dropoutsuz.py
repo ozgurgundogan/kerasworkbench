eda886e338451d65735634a839dfac0af15002fa
ozgur <ozgurgundogan90@gmail.com>
2017-08-20 11:16:19 +0300

    First commit

--- keras_dropoutsuz.py
+++ keras_dropoutsuz.py
@@ -21,6 +21,7 @@ def remove_zero(s):
     return s[:-1]
 
 for root, dirs, files in os.walk(r"C:\Users\hilmi\Desktop\keras/"): #directory
+#for root, dirs, files in os.walk(r"C:\Users\hilmi\Desktop\keras/"): #directory
     for file in files:  #files in directory
         if file.endswith(".txt"): # finding .txt files
             a,b,c = file.split('_')  #extracting ground truth
@@ -31,6 +32,10 @@ for root, dirs, files in os.walk(r"C:\Users\hilmi\Desktop\keras/"): #directory
             ground_truth = np.array([g1,g2,g3]) # ground truth we obtain from .txt
             my_data = genfromtxt(file, delimiter='\n')
             print (my_data.size) 
-
-            model.compile(loss='mse', optimizer = 'rmsprop',metrics = ['accuracy'])
-            model.fit(my_data,ground_truth,epochs = 1, batch_size = 1)
+            
+            # stochastic gradient descent 
+	    sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+            
+            model.compile(loss='mse', optimizer = sgd ,metrics = ['accuracy'])
+            histry = model.fit(my_data,ground_truth,epochs = 1, batch_size = 1,verbose=2)
+	    print histry
